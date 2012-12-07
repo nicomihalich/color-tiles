72d084db892b4d52a2c1f951b57afddf66ac40d7
Nico Mihalich <nmihalich@gmail.com>
2012-12-07 03:04:38 -0500

    Clicking and updating

--- js/board.js
+++ js/board.js
@@ -1,11 +1,4 @@
-var SIZE = 20;
-
-var colors = {
-	1: "rgb(200,0,0)",
-  2: "rgb(0,200,0)",
-  3: "rgb(0,0,200)",
-  4: "rgb(0,0,0)",
-}
+var SIZE = 25;
 
 function Board(){
 	var squares = [];
@@ -14,14 +7,13 @@ function Board(){
 		for (var i=0;i<SIZE;i++){
 			for (var j=0;j<SIZE;j++){
 				var s = new Square();
-				s.init(i*(SQUARESIZE + 2),j*(SQUARESIZE + 2),colors[Math.floor(Math.random()*4)]);
+				s.init(i*(SQUARESIZE + 2),j*(SQUARESIZE + 2),colors[Math.floor(Math.random()*15)]);
 				squares.push(s);
 			}
 		}
 	}
 
 	this.draw = function(){
-
 		for (var i=0;i<squares.length;i++){
 			if (squares[i] != null)
 				squares[i].draw();
@@ -29,14 +21,36 @@ function Board(){
 	}
 
 	this.updateBoard = function(i){
-
+		if (squares[i].empty == 0)
+			return;
+		var vertIndexForward = i;
+		var horIndexForward = i;
+		var vertIndexBackward = i;
+		var horIndexBackward = i;
+		while (squares[vertIndexForward].empty == 1 && vertIndexForward<squares.length){
+			vertIndexForward++;
+		}
+		while (squares[horIndexForward].empty == 1 && horIndexForward<squares.length){
+			horIndexForward += SIZE;
+		}
+		while (squares[vertIndexBackward].empty == 1 && vertIndexBackward<squares.length){
+			vertIndexBackward--;
+		}
+		while (squares[horIndexBackward].empty == 1 && horIndexBackward<squares.length){
+			horIndexBackward -= SIZE;
+		}
+		squares[vertIndexForward].fill = "rgb(0,0,0)";
+		squares[horIndexForward].fill = "rgb(0,0,0)";
+		squares[vertIndexBackward].fill = "rgb(0,0,0)";
+		squares[horIndexBackward].fill = "rgb(0,0,0)";
 	}
 
 	this.handleMouse = function(mx,my){
 		for (var i=0;i<squares.length;i++){
 			if (squares[i] != null)
 				if (squares[i].isClicked(mx,my)){
-
+					this.updateBoard(i);
+					return;
 				}
 		}
 	}
--- js/square.js
+++ js/square.js
@@ -1,17 +1,40 @@
 var SQUARESIZE = 20;
 
+var colors = {
+	1: "rgb(255,255,255)",
+	2: "rgb(200,0,0)",
+  3: "rgb(0,200,0)",
+  4: "rgb(0,0,200)",
+  5: "rgb(200,200,0)",
+  6: "rgb(0,200,200)",
+  7: "rgb(200,0,200)",
+  8: "rgb(100,50,55)",
+  9: "rgb(100,100,100)",
+
+  //Extra whites
+  10: "rgb(255,255,255)",
+  11: "rgb(255,255,255)",
+  12: "rgb(255,255,255)",
+  13: "rgb(255,255,255)",
+  14: "rgb(255,255,255)",
+  15: "rgb(255,255,255)",
+}
+
 function Square(){
 	
 	this.x = 0;
 	this.y = 0;
 	this.w = SQUARESIZE;
 	this.h = SQUARESIZE;
-	this.fill = "rgb(0,0,0)";
+	this.fill = colors[1];
+	this.empty = 0;
 
 	this.init = function(x,y,fill){
 		this.x = x;
 		this.y = y;
 		this.fill = fill;
+		if (this.fill == colors[1])
+			this.empty = 1;
 	}
 
 	this.draw = function(){
